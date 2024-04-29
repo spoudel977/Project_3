66f850aa1dcabb3657191ed743dffcc7c03e4c6e
spoudel977 <94360634+spoudel977@users.noreply.github.com>
2024-04-29 12:36:35 -0230

    missing data cleaning file - work recompleted and added

---
+++ santosh_p/data_cleaning/api_keys.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+weather_api_key = "037af2f91213659ed438e8f8a09a56a0"
+
+# Geoapify API Key
+geoapify_key = "b78cce50534c4b878677c75348c2509b"
+
+# Google API Key
+g_key = "AIzaSyCR4l1HmEJzOi0Yw57DxQfcV-Mm7wa4W5w"
